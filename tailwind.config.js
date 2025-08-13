5731f02f1d3aa95b2d701481304f5e11a8b6b2c1
Ali <108173040+its4li@users.noreply.github.com>
2025-08-13 19:47:33 +0330

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/app/**/*.{js,jsx}",
+    "./src/components/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "1rem"
+      }
+    },
+  },
+  plugins: [],
+};
